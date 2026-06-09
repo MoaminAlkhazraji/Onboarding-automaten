7623acf20a01a62638def3a73fe88db68c478ce0
MartinH <hellstroemh@gmail.com>
2026-06-09 12:00:21 +0200

    implementerar Write-Log funktionen

--- Onboarding-Script.ps1
+++ Onboarding-Script.ps1
@@ -1,11 +1,36 @@
+# FUNKTIONER
+function Write-Log {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Message,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateSet("INFO","SUCCESS","WARNING","ERROR")]
+        [string]$Level = "INFO"
+    )
+    
+    $Timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $LogEntry = "[$Timestamp] [$Level] $Message"
+
+
 # Skriv till Loggfil
-$Logpath = "C:\Logs\Onboarding\Onboarding_$(Get-Date -Format 'yyyy-MM-dd').log"
+$LogPath = "C:\Logs\Onboarding\Onboarding_$(Get-Date -Format 'yyyy-MM-dd').log"
 try {
     if (-not (Test-Path (Split-Path $Logpath))) {
-        New-item -Itemtype Directory -Path (Split-Path $Logpath) -Force | Out-Null
+        New-item -Itemtype Directory -Path (Split-Path $LogPath) -Force | Out-Null
     }
     Add-Content -Path $LogPath -Value $LogEntry -Encoding UTF8
 }
 catch {
     Write-Warning "Kunde inte skriva till loggfil!"
+}
+
+# Skriv till konsoll med färg
+switch ($Level) {
+    "INFO"    { Write-Host $LogEntry -ForegroundColor Cyan }
+    "SUCCESS" { Write-Host $LogEntry -ForegroundColor Green }
+    "WARNING" { Write-Host $LogEntry -ForegroundColor Yellow }
+    "ERROR"   { Write-Host $LogEntry -ForegroundColor Red }
+    }
 }
