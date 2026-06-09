516fd8f68d2c44cdc774b77d39fd50ef19318e3d
ghostprotocol-rrts <ghostprotocolgithub@gmail.com>
2026-06-09 14:19:03 +0200

    Add files via upload

--- Api request.ps1
+++ Api request.ps1
@@ -1,4 +1,4 @@
-$uri = "Api here"
+$uri = "https://script.google.com/macros/s/AKfycbwFnx_-ZwAeEszfJ9Z72MDfkRddqQsNiVbt6VAlIPftcpvf9zFkYYy8UzYkFV-BPwU/exec?token=ITSEC2026-onboarding-test-token"
 $webRequest = Invoke-WebRequest -Uri $Uri -Method Get
 
 $webRequest.Content | ConvertFrom-Json
