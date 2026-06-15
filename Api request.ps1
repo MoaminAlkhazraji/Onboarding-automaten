513ea967aa30289c6c2c6476a693f0a8a5b6912b
ghostprotocol-rrts <ghostprotocolgithub@gmail.com>
2026-06-15 11:46:43 +0200

    Update Api request.ps1
    Addaded Variables and added more comments

--- Api request.ps1
+++ Api request.ps1
@@ -1,2 +1,6 @@
+#Api key
 $uri = "API-Key-Here"
+#Hemtar infromtion från api.
 Invoke-RestMethod -Uri $Uri -Method Get
+#Variable för att spara infomationen
+$data = Invoke-RestMethod -Uri $Uri -Method Get
