ffe333abb23d5463fd9a516bb263f0057ddcf082
ghostprotocol-rrts <ghostprotocolgithub@gmail.com>
2026-06-15 12:00:33 +0200

    Update Api request.ps1
    Exemple hur man använder variablen och comments

--- Api request.ps1
+++ Api request.ps1
@@ -4,3 +4,15 @@ $uri = "API-Key-Here"
 Invoke-RestMethod -Uri $Uri -Method Get
 #Variable för att spara infomationen
 $data = Invoke-RestMethod -Uri $Uri -Method Get
+
+
+#Exemple hur man kan avnända $data variablen.
+$data.rowId                    #rowID
+$data.firstName                # första namn
+$data.lastName                 # sista namn
+$data.department               # ex: "ekonomi
+$data.manager                  # 
+
+$data[0].rowId                 # första 
+$data[1].rowId                 # andra
+$data[-1].rowId                # sista
